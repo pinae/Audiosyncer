fc81751464818a4d62a05580ac6a33f633a46ab3
pinae <jonny@pinae.net>
2016-02-09 17:39:12 +0100

    created translation files

---
+++ audiosyncer.pro
@@ -0,0 +1,2 @@
+SOURCES = gui.py
+TRANSLATIONS = i18n/en_GB.ts  i18n/de_DE.ts
---
+++ i18n/en_GB.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="gui.py" line="17"/>
+        <source>Open Clip</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="19"/>
+        <source>Video Files (*.mp4 *.ogv *.mkv *.avi *.mov);;Audio Files (*.flac *.ogg *.wav *.mp3)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="24"/>
+        <source>Open Audio</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="26"/>
+        <source>Audio Files (*.flac *.ogg *.wav *.mp3)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
